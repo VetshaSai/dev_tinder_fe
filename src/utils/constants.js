bea428db68c13deda673fb6bdee75dbcb1930b24
Sai Durgesh <durgarao.vetsha@gmail.com>
2025-01-31 20:02:42 +0530

    base url changed dynamically for both local and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
-//export const BASE_URL = "http://localhost:7777";
-export const BASE_URL = "/api";
+// these two are backend api host address
+//export const BASE_URL = "http://localhost:7777"; -- for local 
+//export const BASE_URL = "/api"; -- for prod
+
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api";
